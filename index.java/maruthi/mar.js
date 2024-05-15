d955df6a87c0f0f4894db2a82b83e69149655c3b
naresh602306 <deannaresh12@gmail.com>
2024-05-15 15:13:47 +0530

    hi

---
+++ index.java/maruthi/mar.js
@@ -0,0 +1,12 @@
+function validateage(){
+    
+    var name =document.getElementById("name");
+    var age =document.getElementById("age").value;
+    var result=document.querySelector("p");
+
+     if(age > 18 ){
+        result.innerHTML="you are elegeble to vote";
+     }else{
+        result.innerHTML="you are child😂😊🤣";
+     }
+    }
